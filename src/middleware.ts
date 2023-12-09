ed05b3cd938fbb2d0b1fa8fdd53527dcdca0e805
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-09 15:59:49 -0500

    test deploy alreadyRewritten

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
 import { i18nRouter } from 'next-i18n-router'
 import i18nConfig from './i18nConfig'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
     console.log(i18nRouter(request, i18nConfig))
-    console.log(request.headers.get('x-middleware-rewrite'))
+    console.log(request.headers.get('x-next-i18n-router-locale'))
 
-    const alreadyRewritten = request.headers.get('x-middleware-rewrite')
+    const alreadyRewritten = request.headers.get('x-next-i18n-router-locale')
+    if (alreadyRewritten) {
+        return NextResponse.next()
+    }
 
     return i18nRouter(request, i18nConfig)
 }
