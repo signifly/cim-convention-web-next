e1e8c6cfb8782b0dacae7e3d811e9e33b98a738f
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-09 14:24:30 -0500

    Hello husky?

--- src/app/[locale]/(home-page)/page.tsx
+++ src/app/[locale]/(home-page)/page.tsx
@@ -3,10 +3,12 @@ export default function Home({
 }: {
   params: { locale: string }
 }) {
+  //test changes with husky
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-5xl font-bold">Home Page</h1>
-			<p className="text-xl">Locale: {locale}</p>
+      <h1 className="text-5xl font-bold">Home Page</h1>
+      <p className="text-xl">Locale: {locale}</p>
     </main>
   )
 }
