a3dd4701c913047a9fbe917bea8e8a15215f2055
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-09 14:23:46 -0500

    add clsxMerge.ts

---
+++ src/app/[locale]/[...slug]/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Header } from '@/blocks/Header/Header'
+import { Footer } from '@/blocks/Footer/Footer'
+
+export default function HomePageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
