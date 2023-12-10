31507eee76b413fdd6026914a6e88457ac3e40db
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-10 17:38:21 -0500

    feat(HeroBlock): done

--- src/blocks/HeroBlock/HeroBlock.tsx
+++ src/blocks/HeroBlock/HeroBlock.tsx
@@ -16,35 +16,41 @@ export function HeroBlock(props: HeroBlockRecord) {
   } = props
 
   return (
-    <section id={anchorId} className="relative bg-brand-grey-100">
-      <GridContainer className="z-10 block pt-8 lg:grid">
-        <div className="mb-6 flex h-min items-center justify-start text-[18px]">
+    <section
+      id={anchorId}
+      className="relative overflow-y-hidden bg-brand-grey-100"
+    >
+      <GridContainer className="z-10 block pt-8 lg:grid lg:pb-20 lg:pt-24">
+        <div className="mb-6 flex h-min items-center justify-start text-18 lg:col-span-4 lg:text-20">
           <p className="border-r-2 border-brand-grey-300 pr-4 text-brand-green">
             {location}
           </p>
           <p className="pl-4 text-brand-grey-400">{date}</p>
         </div>
-        <h1 className="mb-4 text-[2rem] font-[600] leading-[125%] tracking-tight">
+        <h1 className="mb-4 text-32 font-[600] leading-[125%] tracking-tight lg:col-span-6 lg:col-start-1 lg:mb-6 lg:text-48">
           <span className="bg-brand-gradient bg-clip-text text-transparent">
             {emphasizedHeadlineText}
           </span>{' '}
           {restOfHeadline}
         </h1>
-        <p className="mb-10 leading-[150%] text-brand-grey-600">
+        <p className="mb-10 leading-[150%] text-brand-grey-600 lg:col-span-6 lg:col-start-1 lg:mb-16">
           {subHeadline}
         </p>
-        <div className="mb-4 text-xs text-brand-grey-600">{sponsorHeading}</div>
+        <div className="mb-4 text-xs text-brand-grey-600 lg:col-span-3 lg:col-start-1">
+          {sponsorHeading}
+        </div>
         {sponsorLogo?.responsiveImage && (
           <DatoImage
             data={sponsorLogo.responsiveImage}
-            pictureClassName="h-8 w-20 object-contain object-left"
+            className="lg:col-span-3 lg:col-start-1 lg:h-9 lg:w-24"
+            pictureClassName="h-8 w-20 object-contain object-left lg:w-24 lg:h-9"
           />
         )}
       </GridContainer>
-      {/* <div className="relative lg:absolute lg:right-0 lg:top-0"> */}
-      {/*   <DatoImage data={image.responsiveImage} className="relative" /> */}
-      {/*   <div className="absolute left-0 right-0 top-0 h-1/2 border-green-400 bg-gradient-to-b from-brand-grey-100 to-transparent" /> */}
-      {/* </div> */}
+      <div className="relative lg:absolute lg:right-0 lg:top-0 lg:h-full lg:w-1/2">
+        <DatoImage data={image.responsiveImage} className="relative" />
+        <div className="absolute left-0 right-0 top-0 h-1/2 bg-gradient-to-b from-brand-grey-100 to-transparent lg:left-[unset] lg:h-full lg:w-full lg:bg-gradient-to-l lg:from-transparent lg:to-brand-grey-100" />
+      </div>
     </section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from 'tailwindcss'
 
+const BASE_FONT_SIZE = 16
+const rem = (px, key = px) => ({ [key]: `${px / BASE_FONT_SIZE}rem` })
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -44,6 +47,19 @@ const config: Config = {
       },
       boxShadow: {
         xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
+      },
+      fontSize: {
+        ...rem(11),
+        ...rem(12),
+        ...rem(14),
+        ...rem(16),
+        ...rem(18),
+        ...rem(20),
+        ...rem(24),
+        ...rem(28),
+        ...rem(32),
+        ...rem(40),
+        ...rem(48),
       },
     },
   },
