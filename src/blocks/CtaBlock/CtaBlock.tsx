0905feedb5532e6db2ab771175ea71ebee6f7ecf
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-22 00:10:16 -0500

    fix(CtaBlock): add 80px padding for lg

--- src/blocks/CtaBlock/CtaBlock.tsx
+++ src/blocks/CtaBlock/CtaBlock.tsx
@@ -11,7 +11,7 @@ export const CtaBlock = (props: CtaBlockRecord) => {
       id={anchorId ?? ''}
       className="bg-gradient-to-b from-brand-grey-200 via-white/50 to-white/50"
     >
-      <GridContainer className="grid-cols-1 place-items-center py-14 text-center lg:grid-cols-1">
+      <GridContainer className="grid-cols-1 place-items-center py-14 text-center lg:grid-cols-1 lg:py-20">
         <h2 className="mb-4 text-28/[125%] font-semibold tracking-[-0.14px] text-brand-grey-950 lg:text-40/[115%]">
           {headline}
         </h2>
