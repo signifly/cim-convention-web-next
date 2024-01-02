9f4afae8d8c6042cd6147c86bc0ba3ab85d88910
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-02 16:22:27 -0500

    fix(default-header-block): remove unused fragment

--- src/blocks/DefaultHeaderBlock/DesktopMenuContent.tsx
+++ src/blocks/DefaultHeaderBlock/DesktopMenuContent.tsx
@@ -13,58 +13,56 @@ export const DesktopMenuContent = ({ menu }: { menu: MenuRecord }) => {
         if (subMenuItems.length > 0) {
           return (
             <Popover key={item.id} className="relative">
-              <>
-                <Popover.Button className="flex items-center gap-x-1 font-medium leading-6 text-gray-900">
-                  {item.name}
-                  <ChevronDownIcon
-                    className="h-5 w-5 flex-none text-gray-400"
-                    aria-hidden="true"
-                  />
-                </Popover.Button>
+              <Popover.Button className="flex items-center gap-x-1 font-medium leading-6 text-gray-900">
+                {item.name}
+                <ChevronDownIcon
+                  className="h-5 w-5 flex-none text-gray-400"
+                  aria-hidden="true"
+                />
+              </Popover.Button>
 
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel
+                  static
+                  className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5"
                 >
-                  <Popover.Panel
-                    static
-                    className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5"
-                  >
-                    <div className="p-4">
-                      {subMenuItems.map((subMenu) => {
-                        return (
-                          subMenu.linkTo && (
-                            <div
-                              key={subMenu.id}
-                              className="group relative flex items-center gap-x-6 rounded-lg p-4 leading-6 hover:bg-gray-50"
-                            >
-                              <div className="flex-auto">
-                                <Link
-                                  href={{
-                                    pathname: '/[...slug]',
-                                    params: {
-                                      slug: [subMenu.linkTo.slug],
-                                    },
-                                  }}
-                                  className="block font-medium text-gray-900"
-                                >
-                                  {subMenu.name}
-                                  <span className="absolute inset-0" />
-                                </Link>
-                              </div>
+                  <div className="p-4">
+                    {subMenuItems.map((subMenu) => {
+                      return (
+                        subMenu.linkTo && (
+                          <div
+                            key={subMenu.id}
+                            className="group relative flex items-center gap-x-6 rounded-lg p-4 leading-6 hover:bg-gray-50"
+                          >
+                            <div className="flex-auto">
+                              <Link
+                                href={{
+                                  pathname: '/[...slug]',
+                                  params: {
+                                    slug: [subMenu.linkTo.slug],
+                                  },
+                                }}
+                                className="block font-medium text-gray-900"
+                              >
+                                {subMenu.name}
+                                <span className="absolute inset-0" />
+                              </Link>
                             </div>
-                          )
+                          </div>
                         )
-                      })}
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
+                      )
+                    })}
+                  </div>
+                </Popover.Panel>
+              </Transition>
             </Popover>
           )
         } else {
