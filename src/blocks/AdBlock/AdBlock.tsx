cb8b35fb65ff26ef147cb23492080df36cc59136
Hugo Miranda <hm@signifly.com>
2024-01-04 13:47:28 -0500

    Feedback fix

--- src/blocks/AdBlock/AdBlock.tsx
+++ src/blocks/AdBlock/AdBlock.tsx
@@ -5,21 +5,28 @@ import { Ad } from '@/components/Ad'
 export const AdBlock = (props: AdBlockRecord) => {
   const { anchorId, banner, ad, ads } = props
 
-  let sectionClasses = 'py-6 lg:py-16'
-  let gridClasses = 'grid-cols-1'
-  let divClasses =
-    'mx-10 flex flex-col gap-12 lg:col-span-10 lg:col-start-2 lg:mx-0 lg:flex-row'
-
-  if (banner) {
-    sectionClasses = 'bg-brand-grey-25 py-10 lg:py-16'
-    gridClasses = 'lg:place-items-center'
-    divClasses = 'col-span-4 lg:col-span-8 lg:col-start-3 lg:px-8'
+  const styles = {
+    section: {
+      banner: 'bg-brand-grey-25 py-10 lg:py-16',
+      default: 'py-6 lg:py-16',
+    },
+    grid: {
+      banner: 'lg:place-items-center',
+      default: 'grid-cols-1',
+    },
+    div: {
+      banner: 'col-span-4 lg:col-span-8 lg:col-start-3 lg:px-8',
+      default:
+        'mx-10 flex flex-col gap-12 lg:col-span-10 lg:col-start-2 lg:mx-0 lg:flex-row',
+    },
   }
 
+  const styleOption = banner ? 'banner' : 'default'
+
   return (
-    <section id={anchorId || ''} className={sectionClasses}>
-      <GridContainer className={gridClasses}>
-        <div className={divClasses}>
+    <section id={anchorId || ''} className={styles.section[styleOption]}>
+      <GridContainer className={styles.grid[styleOption]}>
+        <div className={styles.div[styleOption]}>
           {banner
             ? ad && <Ad {...ad} />
             : ads && ads.map((each) => <Ad key={each.id} {...each} />)}
