b3a33e6c4005a5419c130c71a9d0644ee168011f
Hugo Miranda <hm@signifly.com>
2024-01-07 13:31:23 -0500

    Registration Tier mobile responsiveness

--- src/components/RegistrationTier.tsx
+++ src/components/RegistrationTier.tsx
@@ -41,8 +41,10 @@ export const RegistrationTier = (
           onClick={toggleDetails}
           className="flex w-[100%] cursor-pointer justify-between"
         >
-          <h3 className="text-24/[140%] font-medium">{title}</h3>
-          <div className="h-[calc(24px * 1.4)] flex items-center justify-center p-2">
+          <h3 className="text-18/[140%] font-medium md:text-24/[140%]">
+            {title}
+          </h3>
+          <div className="h-[calc(24px * 1.4)] flex items-start justify-center p-2">
             <IconSelector
               selectorId="chevron_icon"
               className={` text-black ${
@@ -52,13 +54,13 @@ export const RegistrationTier = (
           </div>
         </div>
         <div
-          className={`grid pt-6 transition-all duration-300 ${
-            openDetails ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'
+          className={`grid pt-4 transition-all duration-300 lg:pt-6 ${
+            openDetails ? 'grid-rows-[1fr] pb-4 lg:pb-0' : 'grid-rows-[0fr]'
           }`}
         >
           <div
             dangerouslySetInnerHTML={{ __html: details }}
-            className="registration-tier-details overflow-hidden"
+            className="registration-tier-details overflow-hidden text-14/[150%] md:text-16/[150%]"
           />
         </div>
       </div>
@@ -69,12 +71,12 @@ export const RegistrationTier = (
           <div className="flex w-[100%] grow flex-row items-center justify-between border-t border-brand-grey-300 py-2 pl-0 lg:w-[50%] lg:flex-col lg:items-start lg:justify-normal lg:border-l lg:border-t-0 lg:py-0 lg:pl-8">
             <div className="flex flex-col">
               <p className="mb-2 font-medium text-brand-green">{earlyPrice}</p>
-              <p className="text-14/[140%] uppercase text-brand-grey-600">
+              <p className="text-12/[130%] uppercase text-brand-grey-600 md:text-14/[140%]">
                 {earlyLabel}
               </p>
             </div>
             {registrationPeriod === 'early' && (
-              <div className="mt-0 lg:mt-6">
+              <div className="ml-2 mt-0 lg:ml-0 lg:mt-6">
                 <CtaButton {...earlyButton[0]} />
               </div>
             )}
@@ -83,12 +85,12 @@ export const RegistrationTier = (
           <div className="flex w-[100%] grow flex-row items-center justify-between border-t border-brand-grey-300 py-2 pl-0 lg:w-[50%] lg:flex-col lg:items-start lg:justify-normal lg:border-l lg:border-t-0 lg:py-0 lg:pl-8">
             <div className="flex flex-col">
               <p className="mb-2 font-medium">{latePrice}</p>
-              <p className="text-14/[140%] uppercase text-brand-grey-600">
+              <p className="text-12/[130%] uppercase text-brand-grey-600 md:text-14/[140%]">
                 {lateLabel}
               </p>
             </div>
             {registrationPeriod === 'late' && (
-              <div className="mt-0 lg:mt-6">
+              <div className="ml-1 mt-0 lg:ml-0 lg:mt-6">
                 <CtaButton {...lateButton[0]} />
               </div>
             )}
