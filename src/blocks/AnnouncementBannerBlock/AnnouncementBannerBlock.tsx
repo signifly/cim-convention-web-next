7b1db9bd06a206b6b2ef153868088338018f1d2b
Hugo Miranda <hm@signifly.com>
2024-01-11 17:21:08 -0500

    Fixed announcement banner

--- src/blocks/AnnouncementBannerBlock/AnnouncementBannerBlock.tsx
+++ src/blocks/AnnouncementBannerBlock/AnnouncementBannerBlock.tsx
@@ -8,7 +8,7 @@ export const AnnouncementBannerBlock = (
 
   return (
     <section
-      className="bg-brand-grey-800 py-[10px] text-center text-11/[140%] text-white lg:text-14"
+      className="announcement-banner fixed top-0 z-50 h-[40px] w-[100%] overflow-hidden bg-brand-grey-800 py-[10px] text-center text-11/[140%] text-white lg:text-14"
       aria-label="announcement banner"
     >
       <div className="container mx-auto">{text}</div>
