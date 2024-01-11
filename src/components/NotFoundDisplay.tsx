07f52a99e744db92446a01edd5304793dc3889b2
Duy Nguyen <duythedeveloper@gmail.com>
2024-01-10 19:23:27 -0500

    feat(404): add 404 not found display page to all routes (#53)
    * feat(404): add 404 not found display page to all routes
    
    * fix(cleanup): fix ts error in CTAButton.tsx and cleanup unused code

--- src/components/NotFoundDisplay.tsx
+++ src/components/NotFoundDisplay.tsx
@@ -1,5 +1,39 @@
 import React from 'react'
+import { useLocale } from 'next-intl'
+import { getPageBySlugQuery } from '@/lib/datocms/queries/getPageBySlugQuery'
+import { ComponentParser, fetchDatoContent } from '@/lib/datocms'
+import { Locale } from '@/navigation'
 
-export const NotFoundDisplay = () => {
-  return <div>NotFoundDisplay</div>
+export const NotFoundDisplay = async () => {
+  const currentLocale = useLocale() as Locale
+  const { data: pageData } = await fetchDatoContent(
+    getPageBySlugQuery({
+      locale: currentLocale,
+      slug: 'not-found',
+    }),
+  )
+
+  return (
+    <>
+      {pageData?.page?.header?.blocks.map(
+        (block: any) =>
+          block.id && <ComponentParser key={block.id} data={block} />,
+      )}
+      <main className="flex grow flex-col [&>section]:flex [&>section]:min-h-[460px] [&>section]:flex-col [&>section]:justify-center">
+        {pageData?.page?.body?.map(
+          (block: any) =>
+            block.id && <ComponentParser key={block.id} data={block} />,
+        )}
+      </main>
+      {pageData?.page?.footer?.blocks.map(
+        (block: any) =>
+          block.id && (
+            <ComponentParser
+              key={block.id}
+              data={{ ...block, socialLinks: pageData.socialLink }}
+            />
+          ),
+      )}
+    </>
+  )
 }
