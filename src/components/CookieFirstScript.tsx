c41c7cf1a231663fdc5b7ca499c87b8dfcc1af8a
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-17 15:23:25 -0500

    test 5: reload by DOM manipulation

--- src/components/CookieFirstScript.tsx
+++ src/components/CookieFirstScript.tsx
@@ -8,11 +8,24 @@ const CookieFirstScript = () => {
       'cookiefirst-policy-page',
     )
     const head = document.querySelector('head')
+    const body = document.querySelector('body')
     const cookieFirstScript = document.getElementById('cookiefirst-script')
+    const newScript = document
+      .createElement('script')
+      .setAttribute('id', 'cookiefirst-script')
+      .setAttribute(
+        'src',
+        `https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.NEXT_PUBLIC_COOKIE_FIRST_API_KEY}/consent.js`,
+      )
 
     if (!cookiePolicyContainer || !head || !cookieFirstScript) return
-    head?.removeChild(cookieFirstScript)
-    head.innerHTML += `<script id="cookiefirst-script" src="https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.NEXT_PUBLIC_COOKIE_FIRST_API_KEY}/consent.js" defer></script>`
+    try {
+      head.removeChild(cookieFirstScript)
+      body.appendChild(newScript)
+    } catch (err) {
+      console.error(err)
+      return
+    }
   }
 
   useEffect(reloadWidget, [])
