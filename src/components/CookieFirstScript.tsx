ca9367424b4af8aadf2d6abe2fb6d33b017d86f8
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-17 14:10:40 -0500

    revert previous commit

--- src/components/CookieFirstScript.tsx
+++ src/components/CookieFirstScript.tsx
@@ -1,24 +1,9 @@
-'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Script from 'next/script'
-import { useRouter } from '@/navigation'
 
 const CookieFirstScript = () => {
-  const router = useRouter()
-  const reloadWidget = () => {
-    const cookiePolicyContainer = document.getElementById(
-      'cookiefirst-policy-page',
-    )
-
-    if (!cookiePolicyContainer) return
-    router.refresh()
-  }
-
-  useEffect(reloadWidget, [])
-
   return (
     <Script
-      id="cookiefirst-script"
       src={`https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.COOKIE_FIRST_API_KEY}/consent.js`}
     />
   )
