98909029b28f052315024f6fea1e902d3e7fe181
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-17 14:24:54 -0500

    test2: reload cookie script on policy page

--- src/components/CookieFirstScript.tsx
+++ src/components/CookieFirstScript.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import Script from 'next/script'
+import { useRouter } from '@/navigation'
 
 const CookieFirstScript = () => {
+  const router = useRouter()
+
+  const reloadWidget = () => {
+    const cookiePolicyContainer = document.getElementById(
+      'cookiefirst-policy-page',
+    )
+
+    if (!cookiePolicyContainer) return
+    router.refresh()
+  }
+
+  useEffect(reloadWidget, [])
+
   return (
     <Script
-      src={`https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.COOKIE_FIRST_API_KEY}/consent.js`}
+      id="cookiefirst-script"
+      src={`https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.NEXT_PUBLIC_COOKIE_FIRST_API_KEY}/consent.js`}
+      defer
     />
   )
 }
