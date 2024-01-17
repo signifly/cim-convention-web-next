569c116976e0a1a1933fa665d2697c0a706fd749
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-17 14:55:12 -0500

    test 3: reload manually by DOM manipulation

--- src/components/CookieFirstScript.tsx
+++ src/components/CookieFirstScript.tsx
@@ -1,18 +1,18 @@
 'use client'
 import React, { useEffect } from 'react'
 import Script from 'next/script'
-import { useRouter } from '@/navigation'
 
 const CookieFirstScript = () => {
-  const router = useRouter()
-
   const reloadWidget = () => {
     const cookiePolicyContainer = document.getElementById(
       'cookiefirst-policy-page',
     )
+    const head = document.querySelector('head')
+    const cookieFirstScript = document.getElementById('cookiefirst-script')
 
-    if (!cookiePolicyContainer) return
-    router.refresh()
+    if (!cookiePolicyContainer || !head || !cookieFirstScript) return
+    head?.removeChild(cookieFirstScript)
+    head.innerHTML += `<script id="cookiefirst-script" src="https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.NEXT_PUBLIC_COOKIE_FIRST_API_KEY}/consent.js" defer></script>`
   }
 
   useEffect(reloadWidget, [])
@@ -21,7 +21,6 @@ const CookieFirstScript = () => {
     <Script
       id="cookiefirst-script"
       src={`https://consent.cookiefirst.com/sites/convention.cim.org-${process.env.NEXT_PUBLIC_COOKIE_FIRST_API_KEY}/consent.js`}
-      defer
     />
   )
 }
